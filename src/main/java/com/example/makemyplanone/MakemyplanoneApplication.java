cd33bbfa44e496a25e75554c83b8540750cdae82
abc <vaibhavi.warke79@gmail.com>
2019-11-18 12:16:53 +0530

    Initial Commit

---
+++ src/main/java/com/example/makemyplanone/MakemyplanoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.makemyplanone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakemyplanoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakemyplanoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/makemyplanone/MakemyplanoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makemyplanone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemyplanoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
